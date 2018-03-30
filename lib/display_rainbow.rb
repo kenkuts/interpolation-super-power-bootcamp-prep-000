a0c84092f7b99d7006f0121eba49a46d4ef15d6e
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-03-30 04:41:21 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,12 +1,10 @@
-# Write your #display_rainbow method here
 def display_rainbow(array)
   initial_char = array.map{|color| color.split('').first.upcase}
     
   output = []
   
-  for i in 0...array
-    output.push(initial_char[i])
-    output.push(array[i])
+  for i in 0...array.length
+    output.push("#{initial_char[i]}: #{array[i]}")
   end 
   
   puts output.join(", ");
