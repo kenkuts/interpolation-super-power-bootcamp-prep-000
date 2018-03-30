930ba3e6f679d312162c80f015a0e16d9a6dc28f
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-03-30 04:22:35 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,8 @@
 # Write your #display_rainbow method here
 def display_rainbow(array)
-  
+  initial_char = array.select do |color|
+      color.each_with_index do |color, index|
+        return color[index] == 0
+      end
+    end  
 end
